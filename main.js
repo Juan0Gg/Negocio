05bce5ca53e408197f8392095eca8f998c2da7a1
Juan G <juanlgonzalez.gg0@gmail.com>
2025-04-04 10:36:52 -0600

    feat(services): implement service cards with JSON and JS dynamic rendering
    - Create JSON data file for services
    - Add JavaScript to fetch and display cards
    - Include new footer section in HTML

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import slider from "./Assets/carrusel.js";
-slider(); //? Llamamos a la funcion slider para que se active el carrusel
-//Tips: Intersection Observer API
+slider();
+
+const $services = document.getElementById("services");
+const $template = document.getElementById("service-template").content;
+const $fragment = document.createDocumentFragment();
+const getData = async () => {
+  const response = await fetch("Assets/data.json");
+  const data = await response.json();
+  console.log(data.servicios);
+  data.servicios.forEach((el) => {
+    $template.querySelector(".card__title").textContent = el.title;
+    $template.querySelector(".card__subtitle").textContent = el.description;
+    let $clone = document.importNode($template, true);
+    $fragment.appendChild($clone);
+  });
+  $services.appendChild($fragment);
+};
+getData();
